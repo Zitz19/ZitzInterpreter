098d4541625cb312e95e0862d1f4837b6ec54c37
Aleksey Klyuchnikov <kam036@campus.mephi.ru>
2024-01-25 02:11:11 +0300

    gitignore added

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/zitz/Labs/ZitzInterpreter
-# Build directory: /home/zitz/Labs/ZitzInterpreter/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
